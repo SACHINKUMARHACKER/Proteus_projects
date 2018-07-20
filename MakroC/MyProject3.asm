310df3416be20203d4e1a475cab92b52ba4f9171
SACHINKUMARHACKER <sachinkum123567@gmail.com>
2018-07-19 21:29:47 -0700

    added assembly codes for pic 16f877a

---
+++ MakroC/MyProject3.asm
@@ -0,0 +1,316 @@
+
+_main:
+
+;MyProject3.c,1 :: 		void main() {
+;MyProject3.c,2 :: 		TRISB.F0=0;
+	BCF        TRISB+0, 0
+;MyProject3.c,3 :: 		TRISB.F1=0;
+	BCF        TRISB+0, 1
+;MyProject3.c,4 :: 		TRISB.F2=0;
+	BCF        TRISB+0, 2
+;MyProject3.c,5 :: 		TRISB.F3=0;
+	BCF        TRISB+0, 3
+;MyProject3.c,6 :: 		TRISB.F4=0;
+	BCF        TRISB+0, 4
+;MyProject3.c,7 :: 		TRISB.F5=0;
+	BCF        TRISB+0, 5
+;MyProject3.c,8 :: 		TRISB.F6=0;
+	BCF        TRISB+0, 6
+;MyProject3.c,9 :: 		TRISB.F7=0;
+	BCF        TRISB+0, 7
+;MyProject3.c,10 :: 		while(1)
+L_main0:
+;MyProject3.c,12 :: 		PORTB.F0=1;
+	BSF        PORTB+0, 0
+;MyProject3.c,13 :: 		Delay_ms(1000);
+	MOVLW      11
+	MOVWF      R11+0
+	MOVLW      38
+	MOVWF      R12+0
+	MOVLW      93
+	MOVWF      R13+0
+L_main2:
+	DECFSZ     R13+0, 1
+	GOTO       L_main2
+	DECFSZ     R12+0, 1
+	GOTO       L_main2
+	DECFSZ     R11+0, 1
+	GOTO       L_main2
+	NOP
+	NOP
+;MyProject3.c,14 :: 		PORTB.F0=0;
+	BCF        PORTB+0, 0
+;MyProject3.c,15 :: 		Delay_ms(1000);
+	MOVLW      11
+	MOVWF      R11+0
+	MOVLW      38
+	MOVWF      R12+0
+	MOVLW      93
+	MOVWF      R13+0
+L_main3:
+	DECFSZ     R13+0, 1
+	GOTO       L_main3
+	DECFSZ     R12+0, 1
+	GOTO       L_main3
+	DECFSZ     R11+0, 1
+	GOTO       L_main3
+	NOP
+	NOP
+;MyProject3.c,16 :: 		PORTB.F1=1;
+	BSF        PORTB+0, 1
+;MyProject3.c,17 :: 		Delay_ms(1000);
+	MOVLW      11
+	MOVWF      R11+0
+	MOVLW      38
+	MOVWF      R12+0
+	MOVLW      93
+	MOVWF      R13+0
+L_main4:
+	DECFSZ     R13+0, 1
+	GOTO       L_main4
+	DECFSZ     R12+0, 1
+	GOTO       L_main4
+	DECFSZ     R11+0, 1
+	GOTO       L_main4
+	NOP
+	NOP
+;MyProject3.c,18 :: 		PORTB.F1=0;
+	BCF        PORTB+0, 1
+;MyProject3.c,19 :: 		Delay_ms(1000);
+	MOVLW      11
+	MOVWF      R11+0
+	MOVLW      38
+	MOVWF      R12+0
+	MOVLW      93
+	MOVWF      R13+0
+L_main5:
+	DECFSZ     R13+0, 1
+	GOTO       L_main5
+	DECFSZ     R12+0, 1
+	GOTO       L_main5
+	DECFSZ     R11+0, 1
+	GOTO       L_main5
+	NOP
+	NOP
+;MyProject3.c,20 :: 		PORTB.F2=1;
+	BSF        PORTB+0, 2
+;MyProject3.c,21 :: 		Delay_ms(1000);
+	MOVLW      11
+	MOVWF      R11+0
+	MOVLW      38
+	MOVWF      R12+0
+	MOVLW      93
+	MOVWF      R13+0
+L_main6:
+	DECFSZ     R13+0, 1
+	GOTO       L_main6
+	DECFSZ     R12+0, 1
+	GOTO       L_main6
+	DECFSZ     R11+0, 1
+	GOTO       L_main6
+	NOP
+	NOP
+;MyProject3.c,22 :: 		PORTB.F2=0;
+	BCF        PORTB+0, 2
+;MyProject3.c,23 :: 		Delay_ms(1000);
+	MOVLW      11
+	MOVWF      R11+0
+	MOVLW      38
+	MOVWF      R12+0
+	MOVLW      93
+	MOVWF      R13+0
+L_main7:
+	DECFSZ     R13+0, 1
+	GOTO       L_main7
+	DECFSZ     R12+0, 1
+	GOTO       L_main7
+	DECFSZ     R11+0, 1
+	GOTO       L_main7
+	NOP
+	NOP
+;MyProject3.c,24 :: 		PORTB.F3=1;
+	BSF        PORTB+0, 3
+;MyProject3.c,25 :: 		Delay_ms(1000);
+	MOVLW      11
+	MOVWF      R11+0
+	MOVLW      38
+	MOVWF      R12+0
+	MOVLW      93
+	MOVWF      R13+0
+L_main8:
+	DECFSZ     R13+0, 1
+	GOTO       L_main8
+	DECFSZ     R12+0, 1
+	GOTO       L_main8
+	DECFSZ     R11+0, 1
+	GOTO       L_main8
+	NOP
+	NOP
+;MyProject3.c,26 :: 		PORTB.F3=0;
+	BCF        PORTB+0, 3
+;MyProject3.c,27 :: 		Delay_ms(1000);
+	MOVLW      11
+	MOVWF      R11+0
+	MOVLW      38
+	MOVWF      R12+0
+	MOVLW      93
+	MOVWF      R13+0
+L_main9:
+	DECFSZ     R13+0, 1
+	GOTO       L_main9
+	DECFSZ     R12+0, 1
+	GOTO       L_main9
+	DECFSZ     R11+0, 1
+	GOTO       L_main9
+	NOP
+	NOP
+;MyProject3.c,28 :: 		PORTB.F4=1;
+	BSF        PORTB+0, 4
+;MyProject3.c,29 :: 		Delay_ms(1000);
+	MOVLW      11
+	MOVWF      R11+0
+	MOVLW      38
+	MOVWF      R12+0
+	MOVLW      93
+	MOVWF      R13+0
+L_main10:
+	DECFSZ     R13+0, 1
+	GOTO       L_main10
+	DECFSZ     R12+0, 1
+	GOTO       L_main10
+	DECFSZ     R11+0, 1
+	GOTO       L_main10
+	NOP
+	NOP
+;MyProject3.c,30 :: 		PORTB.F4=0;
+	BCF        PORTB+0, 4
+;MyProject3.c,31 :: 		Delay_ms(1000);
+	MOVLW      11
+	MOVWF      R11+0
+	MOVLW      38
+	MOVWF      R12+0
+	MOVLW      93
+	MOVWF      R13+0
+L_main11:
+	DECFSZ     R13+0, 1
+	GOTO       L_main11
+	DECFSZ     R12+0, 1
+	GOTO       L_main11
+	DECFSZ     R11+0, 1
+	GOTO       L_main11
+	NOP
+	NOP
+;MyProject3.c,32 :: 		PORTB.F5=1;
+	BSF        PORTB+0, 5
+;MyProject3.c,33 :: 		Delay_ms(1000);
+	MOVLW      11
+	MOVWF      R11+0
+	MOVLW      38
+	MOVWF      R12+0
+	MOVLW      93
+	MOVWF      R13+0
+L_main12:
+	DECFSZ     R13+0, 1
+	GOTO       L_main12
+	DECFSZ     R12+0, 1
+	GOTO       L_main12
+	DECFSZ     R11+0, 1
+	GOTO       L_main12
+	NOP
+	NOP
+;MyProject3.c,34 :: 		PORTB.F5=0;
+	BCF        PORTB+0, 5
+;MyProject3.c,35 :: 		Delay_ms(1000);
+	MOVLW      11
+	MOVWF      R11+0
+	MOVLW      38
+	MOVWF      R12+0
+	MOVLW      93
+	MOVWF      R13+0
+L_main13:
+	DECFSZ     R13+0, 1
+	GOTO       L_main13
+	DECFSZ     R12+0, 1
+	GOTO       L_main13
+	DECFSZ     R11+0, 1
+	GOTO       L_main13
+	NOP
+	NOP
+;MyProject3.c,36 :: 		PORTB.F6=1;
+	BSF        PORTB+0, 6
+;MyProject3.c,37 :: 		Delay_ms(1000);
+	MOVLW      11
+	MOVWF      R11+0
+	MOVLW      38
+	MOVWF      R12+0
+	MOVLW      93
+	MOVWF      R13+0
+L_main14:
+	DECFSZ     R13+0, 1
+	GOTO       L_main14
+	DECFSZ     R12+0, 1
+	GOTO       L_main14
+	DECFSZ     R11+0, 1
+	GOTO       L_main14
+	NOP
+	NOP
+;MyProject3.c,38 :: 		PORTB.F6=0;
+	BCF        PORTB+0, 6
+;MyProject3.c,39 :: 		Delay_ms(1000);
+	MOVLW      11
+	MOVWF      R11+0
+	MOVLW      38
+	MOVWF      R12+0
+	MOVLW      93
+	MOVWF      R13+0
+L_main15:
+	DECFSZ     R13+0, 1
+	GOTO       L_main15
+	DECFSZ     R12+0, 1
+	GOTO       L_main15
+	DECFSZ     R11+0, 1
+	GOTO       L_main15
+	NOP
+	NOP
+;MyProject3.c,40 :: 		PORTB.F7=1;
+	BSF        PORTB+0, 7
+;MyProject3.c,41 :: 		Delay_ms(1000);
+	MOVLW      11
+	MOVWF      R11+0
+	MOVLW      38
+	MOVWF      R12+0
+	MOVLW      93
+	MOVWF      R13+0
+L_main16:
+	DECFSZ     R13+0, 1
+	GOTO       L_main16
+	DECFSZ     R12+0, 1
+	GOTO       L_main16
+	DECFSZ     R11+0, 1
+	GOTO       L_main16
+	NOP
+	NOP
+;MyProject3.c,42 :: 		PORTB.F7=0;
+	BCF        PORTB+0, 7
+;MyProject3.c,43 :: 		Delay_ms(1000);
+	MOVLW      11
+	MOVWF      R11+0
+	MOVLW      38
+	MOVWF      R12+0
+	MOVLW      93
+	MOVWF      R13+0
+L_main17:
+	DECFSZ     R13+0, 1
+	GOTO       L_main17
+	DECFSZ     R12+0, 1
+	GOTO       L_main17
+	DECFSZ     R11+0, 1
+	GOTO       L_main17
+	NOP
+	NOP
+;MyProject3.c,44 :: 		}
+	GOTO       L_main0
+;MyProject3.c,45 :: 		}
+L_end_main:
+	GOTO       $+0
+; end of _main
